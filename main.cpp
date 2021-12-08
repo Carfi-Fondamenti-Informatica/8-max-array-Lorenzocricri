55927320f1dd43149cb359dc85e642c3f1bf46e1
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-12-08 23:19:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dimensione=0;
+    cin >> dimensione;
+    float arrey[dimensione], x=0;
+    for(int i=0; i<dimensione; i++) {
+        cin >> x;
+        arrey[i] = x;
+    }
+    cout << risultato(arrey, dimensione);
+    return 0;
 }
