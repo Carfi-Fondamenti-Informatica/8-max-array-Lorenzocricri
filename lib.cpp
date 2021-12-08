fa8ee114f1edc75270848ab046c5cb8ea2e8eb68
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-12-08 23:20:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include "lib.h"
 
+float risultato(float arrey[],int dimensione){
+    float maggiore=0;
+    for(int n=0; n<dimensione; n++){
+        if(arrey[n]>maggiore){
+            maggiore = arrey[n];
+        }
+    }
+    return maggiore;
+}
